a7b0e505f76cd53fb6a3e84ac9384a521915f9a8
David Sturgis <david-sturgis@pluralsight.com>
2023-08-15 01:28:19 +0000

    Work in progress for 2023-08-14

--- elevator.py
+++ elevator.py
@@ -1,7 +1,49 @@
+import copy
+
+
 # imperative shell
 def elevator_function(passengers, starting_point):
-    print(1)
-    return 10
+    # FIXME: Better implementation
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+passengers = {
+    "A": {"origin": 1, "destination": 5},
+    "B": {"origin": 6, "destination": 4},
+}
+
+
+def brute_force(passengers, starting_point):
+    highest_floor = get_highest_floor(passengers, starting_point)
+    lowest_floor = get_lowest_floor(passengers, starting_point)
+
+    visited_floors = []
+    total_moves = 0
+
+    print("starting on", starting_point)
+
+    print("going to", lowest_floor)
+
+    for floor in range(starting_point, lowest_floor, -1):
+        visited_floors.append(floor)
+
+    total_moves += abs(lowest_floor - starting_point)
+
+    print("going to", highest_floor)
+
+    for floor in range(lowest_floor, highest_floor):
+        visited_floors.append(floor)
+
+    total_moves += abs(highest_floor - lowest_floor)
+
+    print("back to", lowest_floor)
+
+    for floor in range(highest_floor, lowest_floor, -1):
+        visited_floors.append(floor)
+
+    total_moves += abs(lowest_floor - highest_floor)
+
+    return visited_floors
 
 
 # functional core
@@ -9,18 +51,32 @@ def get_highest_floor(passengers, starting_point):
     # passenger_name = Tom, characteristics = {origin / destinations}
     ret_val = starting_point
     for passenger_name, characteristics in passengers.items():
-        ret_val = max(ret_val, characteristics['origin'])
-        ret_val = max(ret_val, characteristics['destination'])
+        ret_val = max(ret_val, characteristics["origin"])
+        ret_val = max(ret_val, characteristics["destination"])
 
     return ret_val
 
 
 # functional core
-def get_lowest_floor():
-    pass
+def get_lowest_floor(passengers, starting_point):
+    # passenger_name = Tom, characteristics = {origin / destinations}
+    ret_val = starting_point
+    for passenger_name, characteristics in passengers.items():
+        ret_val = min(ret_val, characteristics["origin"])
+        ret_val = min(ret_val, characteristics["destination"])
+
+    return ret_val
+
 
 # functional core
 # assuming that destination should not be the same as origin
 def get_is_going_up(origin, destination):
-
     return destination >= origin
+
+
+def verify_passengers_picked_up(passengers, current_floor):
+    verified_passengers = copy.deepcopy(passengers)
+    for passenger_name, passenger_characteristics in verified_passengers.items():
+        if passenger_characteristics["origin"] == current_floor:
+            passenger_characteristics["origin"] = None
+    return verified_passengers
--- test_example.py
+++ test_example.py
@@ -1,41 +1,71 @@
-from elevator import *
+from elevator import (
+    elevator_function,
+    get_highest_floor,
+    get_lowest_floor,
+    brute_force,
+    verify_passengers_picked_up,
+)
+
+from pytest import mark
 
 
-def test_accepts_inputs():
+def test_elevator_function_signature():
     passengers = {
-        'Tom': { 'origin': 2, 'destination': 5},
-        'Miles': {'origin': 4, 'destination': 10}
+        "Tom": {"origin": 2, "destination": 5},
+        "Miles": {"origin": 4, "destination": 10},
     }
 
     starting_point = 1
 
-    output = elevator_function(passengers, starting_point)
+    move_list = elevator_function(passengers, starting_point)
 
-    assert output == 10, "output should be 10"
+    for x in move_list:
+        assert int(x) == x
 
-
-
-def test_get_highest_floor():
-    passengers = {
-        'Tom': { 'origin': 2, 'destination': 5},
-        'Miles': {'origin': 4, 'destination': 10}
-    }
-
-    starting_point = 1
-
-    output = get_highest_floor(passengers, starting_point)
-
-    assert output == 10
+    for passenger_name, data in passengers.items():
+        first_origin_index = move_list.index(data["origin"])
+        after_origin_slice = move_list[first_origin_index:]
+        assert data["destination"] in after_origin_slice
 
 
-def test_get_highest_floor():
-    passengers = {
-        'Tom': { 'origin': 2, 'destination': 5},
-        'Miles': {'origin': 4, 'destination': 1}
-    }
+passengersA = {
+    "Tom": {"origin": 2, "destination": 5},
+    "Miles": {"origin": 4, "destination": 10},
+}
 
-    starting_point = 1
+passengersB = {
+    "Tom": {"origin": 2, "destination": 5},
+    "Miles": {"origin": 4, "destination": 1},
+}
 
-    output = get_highest_floor(passengers, starting_point)
 
-    assert output == 5
+@mark.parametrize(
+    ("passengers", "starting_point", "expected_output", "test_function"),
+    [
+        (passengersA, 1, 10, get_highest_floor),
+        (passengersB, 1, 5, get_highest_floor),
+        (passengersB, 20, 20, get_highest_floor),
+        (passengersA, 1, 1, get_lowest_floor),
+        (passengersA, 4, 2, get_lowest_floor),
+        (passengersB, 20, 1, get_lowest_floor),
+    ],
+)
+def test_get_highest_or_lowest_floor(
+    passengers, starting_point, expected_output, test_function
+):
+    output = test_function(passengers, starting_point)
+
+    assert output == expected_output
+
+
+def test_brute_force():
+    starting_point = 2
+    output = brute_force(passengersB, starting_point)
+    # from 2 go down to 1 up to 5 then back down to 1
+    assert output == 1 + 4 + 4
+
+
+def test_verify_passengers_picked_up():
+    current_floor = 2
+    verified_passengers_a = verify_passengers_picked_up(passengersA, current_floor)
+    assert verified_passengers_a["Tom"]["origin"] is None
